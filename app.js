dceb25b944e4a56f12311e05935910a2b02f53ef
RaidelRP <raidelrperez94@gmail.com>
2025-01-23 14:50:39 -0300

    Función para agregar amigos y validar entradas

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    nombre = nombre.trim(); // Eliminar espacios iniciales. Evitar que se quiera crear un nombre vacío con varios espacios
+    if (nombre == "") {
+        alert("Debe ingresar un nombre");
+    } else {
+        amigos.push(nombre);
+    }
+    document.getElementById("amigo").value = "";
+}
