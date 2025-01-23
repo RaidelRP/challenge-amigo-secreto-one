ff9aace042d0305889dae717dce0769c3de55a6e
RaidelRP <raidelrperez94@gmail.com>
2025-01-23 15:36:19 -0300

    Funcion para sortear amigo, generar posicion aleatoria y mostrar el amigo

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo() {
-    let ulAmigos = document.getElementById("listaAmigos");
+function crearLi(idPadre, nombre) {
+    let ul = document.getElementById(idPadre);
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(nombre));
+    ul.appendChild(li);
+}
 
+function agregarAmigo() {
     let nombre = document.getElementById("amigo").value;
     nombre = nombre.trim(); // Eliminar espacios iniciales. Evitar que se quiera crear un nombre vacío con varios espacios
 
@@ -12,13 +17,23 @@ function agregarAmigo() {
     } else {
         if (!amigos.includes(nombre)) {  // Validar que no se haya insertado ya
             amigos.push(nombre);
-            
-            let li = document.createElement("li");
-            li.appendChild(document.createTextNode(nombre));
-            ulAmigos.appendChild(li);
-        }else{
+
+            crearLi("listaAmigos", nombre)
+        } else {
             alert(`${nombre} ya está entre tus amigos`)
         }
     }
     document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+    let posicion = Math.floor(Math.random() * amigos.length);
+    let nombre = amigos[posicion];
+    let mensaje = `Tu amigo secreto es ${nombre}`;
+
+    document.getElementById("resultado").innerHTML = mensaje;
 }
