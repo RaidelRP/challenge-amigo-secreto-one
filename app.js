2bdc9f509293fcbe7cdc6f09a1d15cf6b2c7c824
RaidelRP <raidelrperez94@gmail.com>
2025-01-23 15:07:31 -0300

    Agregar <li> con cada inserción

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 let amigos = [];
 
 function agregarAmigo() {
+    let ulAmigos = document.getElementById("listaAmigos");
+
     let nombre = document.getElementById("amigo").value;
     nombre = nombre.trim(); // Eliminar espacios iniciales. Evitar que se quiera crear un nombre vacío con varios espacios
-    if (nombre == "") {
+
+    if (nombre == "") { // Validar que no se quede vacío
         alert("Debe ingresar un nombre");
     } else {
-        amigos.push(nombre);
+        if (!amigos.includes(nombre)) {  // Validar que no se haya insertado ya
+            amigos.push(nombre);
+            
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(nombre));
+            ulAmigos.appendChild(li);
+        }else{
+            alert(`${nombre} ya está entre tus amigos`)
+        }
     }
     document.getElementById("amigo").value = "";
 }
